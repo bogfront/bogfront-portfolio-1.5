0e50629658fc54c66ff9c910f5cea9fb6fea87c9
Konovchenko Bogdan <konovchenkobv@gmail.com>
2018-11-12 09:36:33 +0300

    master
    Добавил Element

--- src/App.vue
+++ src/App.vue
@@ -4,97 +4,24 @@
   </div>
 </template>
 
-<style lang="less">
-  .App {
-    text-align: center;
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
-    &-logo{
-      animation: App-logo-spin infinite 20s linear;
-      height: 80px;
-    }
-
-    &-header {
-      background-color: #222;
-      height: 150px;
-      padding: 20px;
-      color: white;
-    }
-
-    &-title {
-      font-size: 1.5em;
-    }
-
-    &-intro {
-      font-size: large;
-    }
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
   }
+}
+</script>
 
-  @keyframes App-logo-spin {
-    from { transform: rotate(0deg); }
-    to { transform: rotate(360deg); }
-  }
-
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: 'Roboto', sans-serif;
-  }
-
-  ul {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-  }
-
-  a {
-    color: #444;
-    text-decoration: none;
-  }
-
-  p {
-    font-size: 24px;
-    line-height: 34px;
-    font-weight: 300;
-    color: #333;
-  }
-
-  p a {
-    color: #009688;
-  }
-
-  h1, h2, h3, h4, h5, h6 {
-    font-weight: 400;
-  }
-
-  h3 {
-    font-weight: 300;
-    margin-bottom: 60px;
-    margin-top: 0;
-    padding: 30px 40px;
-    text-transform: uppercase;
-  }
-
-  blockquote {
-    border-left: 5px solid #795548;
-    color: #795548;
-    font-size: 125%;
-    font-weight: 400;
-    margin: 20px 0;
-    padding-left: 1.5rem;
-  }
-
-  strong {
-    font-weight: 500;
-  }
-
-  @media (max-width: 767px) {
-    blockquote {
-      font-size: 16px;
-    }
-
-    p {
-      font-size: 16px;
-      line-height: 22px;
-    }
-  }
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
